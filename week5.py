f7ed737a6f8a561ce02263f15d62c0c7fd96e3b3
Alice Parry <chemistryalice42@gmail.com>
2018-10-11 20:12:55 +0100

    Tweaks and notes
    Correcting type error in pong program. Making notes on the lessons in wk5 on mouse click event handlers and lists.

---
+++ week5.py
@@ -0,0 +1,18 @@
+#python learning 2
+#week 5
+
+#notes from lectures - more on Lists
+#when copying the value of a tuple use list e.g. list(tuple); to create a mutable list
+#list.append(n), list.pop(n), list.index(n), remove(n)
+#pop removes the selected element fromt he list and returns it
+#BUT len(list)
+#in can match numbers or whole strings
+#if x in list - returns t/f
+#for x in list: action - performs the action for every x in list
+#cannot remove from a list that you are iterating over, you have to create a separate list of stuff to remove, then if there is stuff in it remove those things from the origional list
+#OR build a new list of the things you want to keep
+#can store multiple pieces of data about an object in a list e.g ball = [1,2,"green"]; x,y,colour
+#build something that works, test it, change it a little, test, change etc.
+#boolean flags, keep track if something has happened or not
+
+#Practice Exercise for Mouse and List Methods
