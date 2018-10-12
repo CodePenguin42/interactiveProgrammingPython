deb954778f1c98ee02bd1687a858c7942f8490b5
Alice Parry <chemistryalice42@gmail.com>
2018-10-12 13:10:22 +0100

    Started the practice questions
    Enjoying making things into strings of strings, this seems harder and more fun than what the question is asking for.

--- week5.py
+++ week5.py
@@ -16,3 +16,55 @@
 #boolean flags, keep track if something has happened or not
 
 #Practice Exercise for Mouse and List Methods
+#1) print mouse poition to console
+
+#imports
+import simplegui
+
+#globals
+
+
+#helpers and handlers - click event
+def on_click(pos):
+    print pos
+
+#create and register
+frame = simplegui.create_frame("clcik", 400, 400)
+frame.set_mouseclick_handler(on_click)
+
+#start
+frame.start()
+
+#2) print colour of displayed circle on click in circle
+#imports
+import simplegui, math
+
+#globals
+RADIUS = 20
+BALL = [[50, 100, "Red"], [150, 100, "Green"], [250, 100, "Blue"]]
+
+#helper and handler - distance
+def distance(p, q):
+    return math.sqrt((p[0] - q[0])**2 + (p[1] - q[1])**2)
+
+#helpers and handlers - click
+def on_click(pos):
+    for ball in BALL:
+        if distance(pos, ball) < RADIUS:
+            print str(BALL[ball][2])
+
+#helpers and handlers - draw
+def draw(canvas):
+    canvas.draw_circle([BALL[0][0], BALL[0][1]], RADIUS, 1, BALL[0][2], BALL[0][2])
+    canvas.draw_circle([BALL[1][0], BALL[1][1]], RADIUS, 1, BALL[1][2], BALL[1][2])
+    canvas.draw_circle([BALL[2][0], BALL[2][1]], RADIUS, 1, BALL[2][2], BALL[2][2])
+
+#create and register
+frame = simplegui.create_frame("Echo click", 300, 200)
+frame.set_mouseclick_handler(on_click)
+frame.set_draw_handler(draw)
+
+#start
+frame.start()
+
+#okay so look at this a little later and finish the for statment later...
