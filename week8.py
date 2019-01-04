84979e1023a0d88f9b8fb5a6f74be3d24cc22c36
Alice Parry <chemistryalice42@gmail.com>
2019-01-04 21:42:55 +0000

    Notes on week 8
    Introductory notes on sets as well as the start of adding more to the bubble game to handle a set of bubbles and their behaviour. It's amazing how much a few lines of code can do.

---
+++ week8.py
@@ -0,0 +1,182 @@
+# # python learning - 2
+# week 8
+
+# Groups of Sprites - Sets
+    # set - a collection of objects (data) and their properties, unique and ordered so faster to use
+    # name1 = set([thing1, thing2, thing3])
+    # print thing in set -> boolean
+    # name2 = set([thing1, thing1, thing2, thing3]) -> set([thing1, thing2, thing3]) == name1
+    # set methods name.add(), name.remove()
+    # cannot remove something if it is not in the set
+    # if thing1 in name1:
+        # name1.remove(thing1)
+
+    # removing stuff from one set and adding it to another:
+    # instructors = set(['Rixner', 'Warren', 'Greiner', 'Wong'])
+    # print instructors
+    #
+    # def get_rid_of(inst_set, starting_letter):
+    #     remove_set = set([])
+    #     for inst in inst_set:
+    #         if inst[0] == starting_letter:
+    #             remove_set.add(inst)
+    #     inst_set.difference_update(remove_set)
+    #
+    # get_rid_of(instructors, 'W')
+    # print instructors
+    # do not modify the set that you are iterating over, create a set of things that you want to remove then use difference update to remove them
+
+    # need to be able to iterate through sets to draw objects in the game, and to decide if there has been a collision, then score points
+        # Assume there are no asteroid - asteroid collisions....
+
+    # need to iterate through a set of asteroids and see if distance from the centers (missile, asteroid) is <= sum of radi, if it is then add the asteroid to the remove set and difference update.
+        # essentially the same logic for the rock and ship collision, just lose a life, if out of lives end the game
+
+# Groups of Sprites - Collisions for Sprites
+    # assume a spherical chicken in a vacuume.
+    # ability to detect a collision with a Sprite goes inside the sprite class - as that is where the location of the sprite data is
+        # this is a rock and anything, and a missile and anything collision.
+        # A ship is not a sprite as it is not going to need the functionality of being a set
+    # remember make a general sprite collision method then use rock.collide(ship) (iterating over rocks) to detect a collision
+    # group_collide(rocks, ship) -> True if there is a collision; is easier
+        # the group_collide function is the wrapper for the iteration only works for an object colliding with a set
+    # Alternative iteration method for removing:
+        # for s in list(myset):
+            # myset.remove(s)
+        #This works as you are creating a duplicate list of the set and removing stuff from the duplicate
+    # Set to set collisions:
+        # for s in set1:
+            # for e in set2:
+                # group_collide(....)
+                # return the number of collisions, count the number of True's from group_collide(....)
+        # I think that should reduce the set-set collision down into essentially multiple object set collisions
+        # many - many mapping -> many(one - many) mappings
+
+# Practice examples using the bubble shooter game
+# Basic infrastructure for Bubble Shooter
+
+import simplegui, random, math
+
+# Global constants
+WIDTH = 800
+HEIGHT = 600
+FIRING_POSITION = [WIDTH // 2, HEIGHT]
+FIRING_LINE_LENGTH = 60
+FIRING_ANGLE_VEL_INC = 0.02
+BUBBLE_RADIUS = 20
+COLOR_LIST = ["Red", "Green", "Blue", "White"]
+
+# global variables
+firing_angle = math.pi / 2
+firing_angle_vel = 0
+bubble_stuck = True
+
+
+# firing sound
+firing_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/Collision8-Bit.ogg")
+
+
+# helper functions to handle transformations
+def angle_to_vector(ang):
+    return [math.cos(ang), math.sin(ang)]
+
+def dist(p,q):
+    return math.sqrt((p[0]-q[0])**2+(p[1]-q[1])**2)
+
+
+# class defintion for Bubbles
+class Bubble:
+
+    def __init__(self, sound = None):
+        self.pos = list(FIRING_POSITION)
+        self.vel = [0, 0]
+        self.color = random.choice(COLOR_LIST)
+        self.sound = sound
+
+    def update(self):
+        self.pos[0] += self.vel[0]
+        self.pos[1] += self.vel[1]
+        if self.pos[0] <= BUBBLE_RADIUS or self.pos[0] >= WIDTH - BUBBLE_RADIUS:
+            self.vel[0] = - self.vel[0]
+
+    def fire_bubble(self, vel):
+        self.vel = vel
+        if self.sound:
+            self.sound.play()
+
+
+    def is_stuck(self):
+        #return true if bubble is touching the top wall
+        if pos[1] <= BUBBLE_RADIUS:
+            return True
+        else:
+            return False
+        # the syntax they use omits the else section
+        #if pos[1] <= BUBBLE_RADIUS:
+            #return True
+        #return False
+
+    def collide(self, bubble):
+        pass
+
+    def draw(self, canvas):
+        canvas.draw_circle(self.pos, BUBBLE_RADIUS, 1, "White", self.color)
+
+
+# define keyhandlers to control firing_angle
+def keydown(key):
+    global a_bubble, firing_angle_vel, bubble_stuck
+    if simplegui.KEY_MAP["space"] == key:
+        bubble_stuck = False
+        vel = angle_to_vector(firing_angle)
+        a_bubble.fire_bubble([4 * vel[0], -4 * vel[1]])
+    elif simplegui.KEY_MAP["left"] == key:
+        firing_angle_vel += FIRING_ANGLE_VEL_INC
+    elif simplegui.KEY_MAP["right"] == key:
+        firing_angle_vel -= FIRING_ANGLE_VEL_INC
+
+def keyup(key):
+    global firing_angle_vel
+    if simplegui.KEY_MAP["left"] == key:
+        firing_angle_vel -= FIRING_ANGLE_VEL_INC
+    elif simplegui.KEY_MAP["right"] == key:
+        firing_angle_vel += FIRING_ANGLE_VEL_INC
+
+# define draw handler
+def draw(canvas):
+    global firing_angle, a_bubble, bubble_stuck
+
+    # update firing angle
+    firing_angle += firing_angle_vel
+
+    #draw firing line
+    orient = angle_to_vector(firing_angle)
+    upper_endpoint = [FIRING_POSITION[0] + FIRING_LINE_LENGTH * orient[0],
+                      FIRING_POSITION[1] - FIRING_LINE_LENGTH * orient[1]]
+    canvas.draw_line(FIRING_POSITION, upper_endpoint, 4, "White")
+
+    # update a_bubble and check for sticking
+    a_bubble.update()
+
+    if a_bubble.is_stuck():
+        bubble_stuck = True
+        stuck_bubbles.add(a_bubble)
+        a_bubble = Bubble(firing_sound)
+        firing_angle = math.pi / 2
+
+
+    # draw a bubble and stuck bubbles
+    a_bubble.draw(canvas)
+    for b in stuck_bubbles:
+        b.draw(canvas)
+
+# create frame and register handlers
+frame = simplegui.create_frame("Bubble Shooter", WIDTH, HEIGHT)
+frame.set_keydown_handler(keydown)
+frame.set_keyup_handler(keyup)
+frame.set_draw_handler(draw)
+
+# create initial buble and start frame
+a_bubble = Bubble(firing_sound)
+stuck_bubbles = set([])
+frame.start()
